174bdb963072df45bf9d2084fba61cab869c2f0c
Mike Frey <frey.mike@gmail.com>
2012-08-27 23:52:35 -0500

    eyebrow movement, scaling and rotation

--- app/js/views/people.js
+++ app/js/views/people.js
@@ -7,7 +7,11 @@ var People = Backbone.View.extend({
     'change #rngHeadWidth': 'changeHeadWidth',
     'change #rngEyeSpacing': 'changeEyeSpacing',
     'change #rngEyeHeight': 'changeEyeHeight',
-    'change #rngEyeSize': 'changeEyeSize'
+    'change #rngEyeSize': 'changeEyeSize',
+    'change #rngEyebrowSpacing': 'changeEyebrowSpacing',
+    'change #rngEyebrowHeight': 'changeEyebrowHeight',
+    'change #rngEyebrowSize': 'changeEyebrowSize',
+    'change #rngEyebrowAngle': 'changeEyebrowAngle'
   },
 
   initialize: function(options) {
@@ -25,7 +29,14 @@ var People = Backbone.View.extend({
     this.eyeRight = this.r.ellipse(cWidth*1.25, cHeight*0.9, cWidth*0.1, cHeight*0.05)
     this.eyes = this.r.setFinish()
 
+    this.r.setStart()
+    var browWidth = cWidth*0.2
+    this.eyebrowLeft = this.r.rect((cWidth*0.75)-(browWidth/2), cHeight*0.75, browWidth, cHeight*0.05)
+    this.eyebrowRight = this.r.rect((cWidth*1.25)-(browWidth/2), cHeight*0.75, browWidth, cHeight*0.05)
+    this.eyes = this.r.setFinish()
+
     this.model.eyes.on('change', this.updateEyes, this)
+    this.model.eyebrows.on('change', this.updateEyebrows, this)
   },
 
   changeHeadHeight: function(ev) {
@@ -67,6 +78,37 @@ var People = Backbone.View.extend({
     var scale = this.model.eyes.get('scale')
     this.eyeLeft.transform('t'+spacing+','+height+'s'+scale)
     this.eyeRight.transform('t'+(-spacing)+','+height+'s'+scale)
+  },
+
+  changeEyebrowSpacing: function(ev) {
+    var cWidth = this.width / 2
+    var val = (ev.target.value / 100) * cWidth
+    this.model.eyebrows.set('spacing', parseFloat(val, 10))
+  },
+
+  changeEyebrowHeight: function(ev) {
+    var cHeight = this.height / 2
+    var val = (ev.target.value / 100) * cHeight
+    this.model.eyebrows.set('height', parseFloat(val))
+  },
+
+  changeEyebrowSize: function(ev) {
+    var val = ev.target.value / 100
+    this.model.eyebrows.set('scale', val)
+  },
+
+  changeEyebrowAngle: function(ev) {
+    var val = ev.target.value
+    this.model.eyebrows.set('angle', val)
+  },
+
+  updateEyebrows: function() {
+    var spacing = this.model.eyebrows.get('spacing')
+    var height = this.model.eyebrows.get('height')
+    var scale = this.model.eyebrows.get('scale')
+    var angle = this.model.eyebrows.get('angle')
+    this.eyebrowLeft.transform('t'+spacing+','+height+'s'+scale+'r'+angle)
+    this.eyebrowRight.transform('t'+(-spacing)+','+height+'s'+scale+'r'+-angle)
   }
 
 
